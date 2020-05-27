d517609b7074e669746d3b9388fa7895c2855528
许婷 <xuting@xutingdeMacBook-Pro.local>
2020-05-27 10:32:37 +0800

    start a project

---
+++ miaosha_1/src/main/java/com/zheliban/miaosha/controller/DemoController.java
@@ -0,0 +1,39 @@
+package com.zheliban.miaosha.controller;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zheliban.miaosha.result.CodeMsg;
+import com.zheliban.miaosha.result.Result;
+
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "hello aa";
+	}
+	//controller层的两种功能：1、rest api json输出 		 2、页面
+	@RequestMapping("/hello")
+	@ResponseBody
+	public Result<String> hello(){
+		return Result.success("hello.word");
+	}
+	@RequestMapping("/helloError")
+	@ResponseBody
+	public Result<String> helloError(){
+		return Result.error(CodeMsg.SERVER_ERROR);
+	}
+	
+
+}
